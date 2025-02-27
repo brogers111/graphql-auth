8282f880d2fc7df2561a3f9b5331a6d6da70d6e5
BrandonRogers <ber64111@gmail.com>
2025-02-26 19:53:05 -0700

    Create signup mutation

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,16 +1,11 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLID } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
-  fields: () => ({
-    testField: {
-      type: GraphQLString,
-      resolve() {
-        return "Hello, GraphQL!";
-      },
-    },
-  }),
+  fields: {
+    dummyField: { type: GraphQLID },
+  },
 });
 
 module.exports = RootQueryType;
